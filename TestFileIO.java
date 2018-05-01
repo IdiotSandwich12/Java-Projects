eb64308cafc24e34519401971809ec9889a4cf97
timothyjou117 <timothyjou117@gmail.com>
2018-05-01 17:05:39 -0400

    Potatoes

---
+++ TestFileIO.java
@@ -0,0 +1,33 @@
+/*
+
+Timothy Jou
+CO SCI 290
+
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestFileIO{
+
+public static void main(String[] args){
+  
+  String currentLine; //holds current line being read in file
+  String filename = "Test.txt"; //holds the relative path of a file
+  
+  //trying to open a file to read
+  try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+    
+    //read each line in the file until the end of program
+    while((currentLine = br.readLine()) != null){
+      System.out.println(currentLine);
+    }
+    
+    //if there is no file to open, the exception will be caught
+  }catch(IOException e){
+    e.printStackTrace();
+  }
+  
+}
+}
---
+++ TextAdventure.java
@@ -0,0 +1,33 @@
+/*
+
+Timothy Jou
+Steven Lu
+Emmanuel Maravilla
+John Paul de Jesus
+
+*/
+
+import java.util.Scanner;
+
+public class TextAdventure{
+
+public static void main(String[]args){
+  
+  Plot: comedic intro to game > ??? narrator intro > explanation of player situation > large ham intro of big baddy karm
+  
+  System.out.println("???: You ever have those moments where you have no f***ing idea what's going on?");
+  System.out.println("Where you're lost as s**t and just waiting for some);  
+  
+  System.out.println("(Uhh, my head. Last thing I remember, I was browsing dank memes. Where am I?)");
+  
+  System.out.println("???: You don't remember anything huh? Typical. Everyone coming here so far has been like that. Huh? Who am I? I'm the Narrator, but that s**t ain't important.");
+  System.out.println("Narrator: What's important is the reason why you're here. Righto then. Aight kiddo, listen up real close. ")
+  
+  System.out.println("???: AWAKE, LOWLY NORMIE OF THE EARTHLY REALM. IT IS I, THE LORD OF DATA ROBBERY, THE KEEPER OF THE TOME OF A MILLION FACES, THE ALL POWERFUL AND MIGHTY, GERBREKCUZ K'ARM.");
+  
+  int choice = input.nextInt();
+  
+  System.out.println("AH...HMM. WHAT DO YOU GO BY AGAIN? ALL YOU HUMANS APPEAR THE SAME TO ME. NOT THAT IT MATTERS OF COURSE.");
+  System.out.println("YOU'LL SIMPLY BE ANOTHER PATHETIC WEAKLING TO BE CRUSHED. SINCE YOU MADE IT THIS FAR THOUGH, I SUPPOSE I SHOULD AT LEAST KNOW YOUR NAME.");
+  
+  String charName = input.next();
